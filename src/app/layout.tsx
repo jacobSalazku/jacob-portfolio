5498701ffdea3503a58352f117cdfc7d84320a3e
jacobSalazku <Cooljss10@gmail.com>
2024-05-24 20:34:08 +0200

    feature: update glbal css and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="oveflow-hidden mx-auto my-0 flex min-h-screen max-w-[1440px] flex-col items-center scroll-smooth">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
