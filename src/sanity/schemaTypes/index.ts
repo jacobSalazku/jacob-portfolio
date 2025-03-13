b827d3b0ac3f8f1fd174d9235f58255127a8ae30
jacobSalazku <Cooljss10@gmail.com>
2025-03-13 03:52:17 +0100

    refactor: remove unused components and schema types

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,20 +1,14 @@
 import { type SchemaTypeDefinition } from 'sanity';
-import { authorType } from './authorType';
 import { blockContentType } from './blockContentType';
-import { categoryType } from './categoryType';
 import { experienceType } from './experienceType';
 import { featureBlock } from './featureBlockType';
 import { localizedTitleType } from './localizedTitleType';
-import { postType } from './postType';
 import { projectType } from './projectType';
 import { techStackType } from './techStackType';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     blockContentType,
-    categoryType,
-    postType,
-    authorType,
     techStackType,
     projectType,
     experienceType,
