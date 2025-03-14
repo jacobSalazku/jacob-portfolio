f1fb26e477d24fde524d7a139dee47a833712dbb
jacobSalazku <Cooljss10@gmail.com>
2025-03-14 02:30:40 +0100

    feat: adjust header mobile version and resume based on locale

--- src/components/header-banner.tsx
+++ src/components/header-banner.tsx
@@ -3,21 +3,22 @@ import { getTranslations } from 'next-intl/server';
 import '../app/typewriter.css';
 import { Link } from './Link/link';
 
-const Header = async () => {
-  const resume = process.env.NEXT_PUBLIC_RESUME;
+const Header = async ({ locale }: { locale: string }) => {
+  const resumeNl = process.env.NEXT_PUBLIC_RESUME_NL;
+  const resumeEN = process.env.NEXT_PUBLIC_RESUME_EN;
   const t = await getTranslations('Home');
   return (
-    <section className="mt-4 flex w-full flex-col items-center lg:max-w-screen-lg">
-      <div className="flex w-full flex-col items-center pb-4 lg:flex-row lg:justify-between">
-        <div className="flex w-full flex-col items-center justify-center gap-4 px-2 lg:h-[500px] lg:items-start lg:gap-8">
+    <section className="flex h-screen w-full flex-col items-center lg:h-[500px] lg:max-w-screen-lg">
+      <div className="flex h-screen w-full flex-col items-center md:pb-4 lg:flex-row lg:justify-between">
+        <div className="xxxs:justify-start  xxs:justify-center flex h-screen w-full flex-col items-center gap-2 px-2 lg:h-[500px] lg:items-start lg:gap-8">
           <h1
             id="typewriter"
-            className="text-4xl font-light text-beige-1 lg:text-6xl"
+            className="text-3xl font-light text-beige-1 transition-all duration-150 ease-in-out md:text-6xl"
           ></h1>
-          <h2 className="text-2xl font-light text-white lg:text-3xl">
+          <h2 className="text-center text-lg font-light text-white transition-all duration-150 ease-in-out md:text-3xl">
             {t('position')}
           </h2>
-          <p className="flex w-full pb-10 text-center text-2xl font-thin text-white lg:text-left">
+          <p className="flex w-full items-center pb-10 text-center text-sm font-thin text-white transition-all duration-150 ease-in-out sm:text-base md:text-2xl lg:text-left">
             {t('text')}
           </p>
           <Image
@@ -32,14 +33,14 @@ const Header = async () => {
               variant="ghost"
               label="Linkedin"
               href="https://www.linkedin.com/in/jacob-salazaku-8a20142b5/"
-              className="lg:w-1/4"
+              className="lg:w-2/5"
               target="_blank"
             />
             <Link
               variant="primary"
               label={t('cv')}
-              href={resume}
-              className="lg:w-1/4"
+              href={locale === 'nl' ? resumeNl : resumeEN}
+              className="lg:w-2/5"
               target="_blank"
             />
           </div>
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -24,10 +24,10 @@ const Navigation = ({ locale }: { locale: string }) => {
       <header
         className={cn(
           isOpen ? 'hidden' : 'flex',
-          'fixed z-20 h-20 w-full items-center justify-between border-b border-blue-100 border-opacity-50 bg-black-400 px-6 lg:absolute lg:hidden',
+          'fixed z-20 h-20 w-full items-center justify-between border-b border-blue-100 border-opacity-50 bg-black-400 px-4 md:px-10 xl:absolute xl:hidden',
         )}
       >
-        <div className="text-white lg:hidden">
+        <div className="text-white xl:hidden">
           <h3 className="text-base font-bold xl:text-lg">Jacob Salazaku</h3>
         </div>
         <MenuButton onClick={toggleMenu} isActive={isOpen} />
@@ -36,24 +36,24 @@ const Navigation = ({ locale }: { locale: string }) => {
       <nav
         className={cn(
           isOpen
-            ? 'w-full translate-x-0 duration-300 lg:translate-x-0'
-            : '-translate-x-full lg:translate-x-0 ',
-          `fixed left-0 top-0 z-40 h-screen bg-black-400 bg-opacity-70 px-2 py-12 text-white backdrop-blur-md ease-out sm:px-4 lg:sticky lg:-left-3 lg:z-0 lg:w-full lg:max-w-96 lg:px-4 lg:py-12 lg:opacity-100`,
+            ? 'w-full translate-x-0 duration-300 xl:translate-x-0'
+            : '-translate-x-full xl:translate-x-0 ',
+          `fixed left-0 top-0 z-40 h-screen bg-black-400 bg-opacity-70 px-4 py-12 text-white backdrop-blur-md ease-out sm:px-4 md:px-6 xl:sticky xl:-left-3 xl:z-0 xl:w-4/12 xl:max-w-96 xl:px-4 xl:py-12 xl:opacity-100`,
         )}
       >
         {isOpen && (
-          <div className="absolute right-6 top-7 lg:hidden">
+          <div className="absolute right-6 top-7 xl:hidden">
             <MenuButton onClick={toggleMenu} isActive={isOpen} />
           </div>
         )}
         <div className="flex h-full w-full flex-col items-center gap-12 md:items-start">
           <div className="flex w-full items-center gap-2">
-            <div className="flex min-w-16 sm:flex sm:w-full sm:justify-start lg:hidden lg:w-auto">
+            <div className="flex min-w-16 sm:flex sm:w-full sm:justify-start xl:hidden xl:w-auto">
               <h3 className="text-base font-bold xl:text-lg">
                 {t('portfolio')}
               </h3>
             </div>
-            <div className="hidden px-4 lg:block">
+            <div className="hidden px-4 xl:block">
               <h3 className="text-base font-bold xl:text-lg">Jacob Salazaku</h3>
               <p className="text-xs font-thin xl:text-sm">{t('position')}</p>
             </div>
